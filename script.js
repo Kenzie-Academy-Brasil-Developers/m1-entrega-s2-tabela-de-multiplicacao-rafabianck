d5a33dc8cb6d03d5cb85c0b10a7d2156782a9a74
rafabianck <rafabianck@gmail.com>
2022-01-16 23:24:09 -0300

    resolução katas + teste de mesa

---
+++ script.js
@@ -0,0 +1,25 @@
+function tabuada() {
+    let tabuada = [];
+  
+    for (let linha = 0; linha <= 10; linha++) {
+      tabuada.push(new Array(11));
+      for (let coluna = 0; coluna <= 10; coluna++) {
+        tabuada[linha][coluna] = linha * coluna;
+      }
+    }
+    return tabuada;
+  }
+
+// BÔNUS
+
+function tabuadaDinamica(n) {
+    let tabuada = [];
+  
+    for (let linha = 0; linha <= n; linha++) {
+      tabuada.push(new Array(n + 1));
+      for (let coluna = 0; coluna <= n; coluna++) {
+        tabuada[linha][coluna] = linha * coluna;
+      }
+    }
+    return tabuada;
+  }
